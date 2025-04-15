9166f7b05535a33e96bdfab50299b7180c096d8e
Vampire Squad <vampiresquad.org@gmail.com>
2025-04-15 14:34:25 +0600

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -1,45 +1,98 @@
 #!/bin/bash
 
-clear
+# Colors
+R='\e[1;91m' G='\e[1;92m' Y='\e[1;93m' C='\e[1;96m' N='\e[0m'
+
 # Banner
-toilet -f big "Termux-usefull" | lolcat
-echo -e "\e[1;90m       Coded by: Muhammad Shourov (VAMPIRE)\e[0m"
-echo ""
+function banner() {
+  clear
+  toilet -f big "Termux-usefull" | lolcat
+  echo -e "${Y}        Coded by: Muhammad Shourov (VAMPIRE)${N}\n"
+}
+
+# Joke API
+function joke() {
+  curl -s https://icanhazdadjoke.com/ -H "Accept: text/plain"
+}
+
+# IP Tracker
+function iptrack() {
+  read -p "Enter IP/Domain: " ip
+  curl -s ipinfo.io/$ip | lolcat
+}
+
+# Subdomain Finder (with crt.sh)
+function subfinder() {
+  read -p "Enter Domain: " domain
+  echo -e "${C}Collecting Subdomains for $domain...${N}"
+  curl -s "https://crt.sh/?q=%25.$domain&output=json" | jq -r '.[].name_value' | sort -u
+}
+
+# Port Scanner
+function portscan() {
+  read -p "Enter Target IP: " ip
+  nmap -F $ip
+}
+
+# Banner Creator
+function custom_banner() {
+  read -p "Enter your banner text: " btext
+  toilet -f big "$btext" | lolcat
+}
+
+# Vampire Mode
+function vampire_mode() {
+  clear
+  toilet -f big "VAMPIRE" | lolcat
+  echo -e "${R}Welcome to Vampire Mode...${N}"
+  sleep 1
+  cmatrix
+}
 
 # Menu
-echo -e "\e[1;96m[1] System Info"
-echo -e "[2] Network Info"
-echo -e "[3] Terminal Fun"
-echo -e "[4] Clean Storage"
-echo -e "[0] Exit\e[0m"
-echo ""
-read -p $'\e[1;92mSelect an option: \e[0m' choice
+while true; do
+  banner
+  echo -e "${C}1) System Info          2) Network Info"
+  echo -e "3) Battery Info         4) Storage Info"
+  echo -e "5) IP Tracker           6) Port Scanner"
+  echo -e "7) Subdomain Finder     8) Speed Test"
+  echo -e "9) TTS Speak           10) Random Joke"
+  echo -e "11) ASCII Banner       12) Matrix Mode"
+  echo -e "13) Clean Storage      14) Vampire Mode"
+  echo -e "15) Games Menu         16) Custom Banner"
+  echo -e "0) Exit${N}"
+  echo ""
+  read -p $'\e[1;92mSelect Option: \e[0m' ch
 
-case $choice in
-  1)
-    echo -e "\n\e[1;93mFetching system info...\e[0m"
-    neofetch
-    ;;
-  2)
-    echo -e "\n\e[1;93mYour IP:\e[0m"
-    curl -s ifconfig.me
-    echo ""
-    ;;
-  3)
-    echo -e "\n\e[1;95mEnjoy the matrix effect! Press CTRL+C to stop.\e[0m"
-    sleep 1
-    cmatrix || { echo -e "\n\e[1;91mCmatrix not installed. Installing now...\e[0m"; pkg install cmatrix -y && cmatrix; }
-    ;;
-  4)
-    echo -e "\n\e[1;94mCleaning cache...\e[0m"
-    rm -rf /data/data/com.termux/files/usr/tmp/*
-    echo -e "\e[1;92mDone!\e[0m"
-    ;;
-  0)
-    echo -e "\e[1;91mExiting...\e[0m"
-    exit
-    ;;
-  *)
-    echo -e "\e[1;91mInvalid choice!\e[0m"
-    ;;
-esac
+  case $ch in
+    1) neofetch ;;
+    2) curl ifconfig.me ;;
+    3) termux-battery-status ;;
+    4) df -h ;;
+    5) iptrack ;;
+    6) portscan ;;
+    7) subfinder ;;
+    8) speedtest-cli ;;
+    9) read -p "Text to Speak: " speak; termux-tts-speak "$speak" ;;
+    10) joke ;;
+    11) read -p "Enter text: " art; figlet "$art" | lolcat ;;
+    12) cmatrix ;;
+    13) rm -rf /data/data/com.termux/files/usr/tmp/* && echo -e "${G}Cache Cleaned!${N}" ;;
+    14) vampire_mode ;;
+    15)
+      echo -e "${Y}Installing terminal games...${N}"
+      yes | pkg install nsnake bastet moon-buggy ninvaders && clear
+      echo -e "${C}1) nsnake   2) bastet   3) moon-buggy   4) ninvaders${N}"
+      read -p "Choose Game: " g
+      case $g in
+        1) nsnake ;; 2) bastet ;; 3) moon-buggy ;; 4) ninvaders ;;
+        *) echo -e "${R}Invalid${N}" ;;
+      esac ;;
+    16) custom_banner ;;
+    0) echo -e "${R}Exiting...${N}"; exit ;;
+    *) echo -e "${R}Invalid Option${N}" ;;
+  esac
+
+  echo -e "\n${C}Press ENTER to return to menu...${N}"
+  read
+done
