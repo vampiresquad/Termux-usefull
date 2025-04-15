2355a8d7dd729d9ec35d3a903b7abb4f170df497
Vampire Squad <vampiresquad.org@gmail.com>
2025-04-15 14:33:30 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
 clear
-echo -e "\e[1;92m[+] Starting installation...\e[0m"
+echo -e "\e[1;92m[+] Installing Termux-usefull tool...\e[0m"
 
-# Auto Fix for pkg update errors
-echo -e "\e[1;94m[*] Updating packages...\e[0m"
+# Update & Fix Errors
 yes | pkg update && yes | pkg upgrade || apt update --fix-missing
 
-# Essential packages
-packages=(figlet toilet ncurses-utils curl neofetch nano lolcat)
-
-for pkg in "${packages[@]}"; do
-    if ! command -v $pkg &> /dev/null; then
-        echo -e "\e[1;93m[+] Installing $pkg...\e[0m"
-        yes | pkg install "$pkg"
-    else
-        echo -e "\e[1;92m[✔] $pkg already installed\e[0m"
-    fi
+# Essential Packages
+pkgs=(figlet toilet neofetch curl wget nano lolcat cmatrix termux-api dnsutils nmap python git)
+for pkg in "${pkgs[@]}"; do
+  if ! command -v $pkg &> /dev/null; then
+    echo -e "\e[1;93m[+] Installing $pkg...\e[0m"
+    yes | pkg install "$pkg"
+  else
+    echo -e "\e[1;92m[✔] $pkg already installed\e[0m"
+  fi
 done
+
+# Python Modules
+pip install --upgrade pip
+pip install requests speedtest-cli lolcat
 
 chmod +x tool.sh
 
-echo -e "\e[1;92m[✔] Installation Complete!\e[0m"
-echo -e "\e[1;94m[!] Run the tool with: ./tool.sh\e[0m"
+echo -e "\n\e[1;92m[✔] Installation Complete!"
+echo -e "[*] Run the tool using: \e[1;96m./tool.sh\e[0m"
